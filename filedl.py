3bc1250207b4e2592e66b94fb1b1a4b14c9d8fb3
phar <phar@phars-MacBook-Pro.local>
2018-05-08 15:08:57 -0700

    filedl.py writes to files, hex output if hexdump is available

--- filedl.py
+++ filedl.py
@@ -4,6 +4,11 @@ import string
 import os
 import sys
 import argparse
+try:
+	import hexdump
+	dohexdump = 1
+except ImportError:
+	dohexdump = 0
 
 BYTES_PER_ENTRY = 3
 ENTRIES_PER_RESPONSE = 15
@@ -11,6 +16,8 @@ ENTRIES_PER_RESPONSE = 15
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain", action="store",required=1,help="dns file server")
 parser.add_argument("-f", "--file", action="store",required=1, help="filename to download")
+parser.add_argument("-o", "--out", action="store",required=1,default="-", help="filename to write to")
+parser.add_argument("-x", "--hex", action="store",default=0, help="hex output during download")
 
 args = parser.parse_args()
 
@@ -37,14 +44,21 @@ def dns_get_file_data(domain,file,offset):
 	return (datalen,"".join(data))
 
 
-file = []
 stat = 1
 filelen = 0
+if args.out == "-":
+	f = sys.stdout
+else:
+	f = open(args.out,"w")
 while stat:
 	(stat,dat) = dns_get_file_data(args.domain, args.file ,filelen)
-	print stat
+	if  dohexdump and (args.hex != 0) :
+		hexdump.hexdump(dat)
+		print ""
+	else:
+		print "%d bytes." % stat
 	filelen += stat
-	file.append(dat)
-print "".join(file)
+	f.write(dat)
+f.close()
 
 
